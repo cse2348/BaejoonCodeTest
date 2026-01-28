fd4280aee321332d67ec9518258cafa8ca85c155
KIMSEOYEON <128014629+cse2348@users.noreply.github.com>
2026-01-28 11:50:37 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,24 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	String line = br.readLine();
+    	
+    	StringTokenizer st = new StringTokenizer(line);
+    	
+    	int a = Integer.parseInt(st.nextToken());
+    	int b = Integer.parseInt(st.nextToken());
+    	
+    	System.out.println(a+b);
+    	System.out.println(a-b);
+    	System.out.println(a*b);
+    	System.out.println(a/b);
+    	System.out.println(a%b);
+    	
+    	
+    }
+}
